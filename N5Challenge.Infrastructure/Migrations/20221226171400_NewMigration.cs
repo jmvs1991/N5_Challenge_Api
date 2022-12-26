07eec6f52be240e5458491398e67106a292c6b74
José Vásquez <josevasquezc6@gmail.com>
2022-12-26 14:20:37 -0300

    Agregando migraciones

---
+++ N5Challenge.Infrastructure/Migrations/20221226171400_NewMigration.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace N5Challenge.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Permisos",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreEmpleado = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    ApellidoEmpleado = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    FechaPermiso = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permisos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TipoPermisos",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoPermisos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Permisos",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "TipoPermisos",
+                schema: "dbo");
+        }
+    }
+}
