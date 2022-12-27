ec40f7d8d5e02c7f662e0711397644484decb9f0
José Vásquez <josevasquezc6@gmail.com>
2022-12-27 11:28:20 -0300

    Agregando controlador de permisos

---
+++ N5Challenge.QueryApi/Controllers/PermissionController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using N5Challenge.Domain.Entities;
+using N5Challenge.QueryApi.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace N5Challenge.QueryApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PermissionController : ControllerBase
+    {
+        private readonly IPermissionService _permissionService;
+
+        public PermissionController(IPermissionService permissionService)
+        {
+            _permissionService = permissionService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<PermissionsEntity>> Get()
+        {
+            return await _permissionService.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<PermissionsEntity> Get(long id)
+        {
+            return await _permissionService.Get(id);
+        }
+    }
+}
---
+++ N5Challenge.QueryApi/Services/IPermissionService.cs
@@ -0,0 +1,13 @@
+﻿using N5Challenge.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace N5Challenge.QueryApi.Services
+{
+    public interface IPermissionService
+    {
+        public Task<IEnumerable<PermissionsEntity>> Get();
+
+        public Task<PermissionsEntity> Get(long id);
+    }
+}
